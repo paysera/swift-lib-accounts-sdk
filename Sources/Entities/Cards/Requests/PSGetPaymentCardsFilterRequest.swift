2d2ae48cd337b3c65c7edf3b4cf58a13156a2f86
Arvydas Dudavicius <arvydas.dudavicius@gmail.com>
2018-06-20 12:18:19 +0300

    PSGetPaymentCardsFilterRequest init updated with other fields

--- Sources/Entities/Cards/Requests/PSGetPaymentCardsFilterRequest.swift
+++ Sources/Entities/Cards/Requests/PSGetPaymentCardsFilterRequest.swift
@@ -3,29 +3,47 @@ import ObjectMapper
 /// The entity class to get payment cards
 public class PSGetPaymentCardsFilterRequest: Mappable {
     
+    public private(set) var accountNumbers: [String]?
+    public private(set) var cardOwnerId: String?
+    public private(set) var accountOwnerId: String?
     public private(set) var limit: Int?
     public private(set) var offset: Int?
     public private(set) var orderBy: String?
     public private(set) var orderDirection: String?
-    public private(set) var accountNumbers: [String]?
     public private(set) var statuses: [String]?
-    public private(set) var cardOwnerId: String?
-    public private(set) var accountOwnerId: String?
     
     required public init?(map: Map) {
         
     }
     
-    /// init PSGetPaymentCardsFilterRequest. One of account_numbers, card_owner_id or account_owner_id must be filled
+    /// init PSGetPaymentCardsFilterRequest. One of accountNumbers, cardOwnerId or accountOwnerId must be filled
     ///
     /// - Parameters:
-    ///   - accountNumbers: array of user payment cards numbers
+    ///   - accountNumbers: array of user payment cards numbers - optional
     ///   - cardOwnerId: optional
     ///   - accountOwnerId: optional
-    public init(accountNumbers: [String]? = nil, cardOwnerId: String? = nil, accountOwnerId: String? = nil) {
+    ///   - limit: optional
+    ///   - offset: optional
+    ///   - orderBy: optional
+    ///   - orderDirection: optional
+    ///   - statuses: optional
+    public init(accountNumbers: [String]? = nil,
+                cardOwnerId: String? = nil,
+                accountOwnerId: String? = nil,
+                limit: Int? = nil,
+                offset: Int? = nil,
+                orderBy: String? = nil,
+                orderDirection: String? = nil,
+                statuses: [String]? = nil) {
+        
         self.accountNumbers = accountNumbers
         self.cardOwnerId = cardOwnerId
         self.accountOwnerId = accountOwnerId
+        self.limit = limit
+        self.offset = offset
+        self.orderBy = orderBy
+        self.orderDirection = orderDirection
+        self.statuses = statuses
     }
     
     public func mapping(map: Map) {
