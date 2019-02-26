71ba1f58b2ac443b0ff76c437c925b3a9d2c1392
captainOfM <daumantas.stravinskas@gmail.com>
2019-02-26 18:31:50 +0200

    added setAccountDescription endpoint
    and change createAccount return type to Promise<PSAccount>

--- Tests/PayseraAccountsSDKTests/PAyseraAccountsSDKTests.swift
+++ Tests/PayseraAccountsSDKTests/PAyseraAccountsSDKTests.swift
@@ -116,4 +116,22 @@ class AccountsSDKTests: XCTestCase {
         wait(for: [expectation], timeout: 3.0)
         XCTAssertNotNil(object)
     }
+    
+    func testSetAccountDescription() {
+        let expectation = XCTestExpectation(description: "")
+        var object: Any?
+        //insert user id and account number
+        accountsApiClient.setAccountDescription(userId: 1111111,
+                                                accountNumber: "change me",
+                                                description: "modric").done { result in
+                                                    object = result
+            }.catch { error in
+                print(error)
+            }.finally {
+                expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
+        XCTAssertNotNil(object)
+    }
 }
