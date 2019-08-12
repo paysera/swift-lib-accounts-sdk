b29b21e793f7e6631bf34a85bfcdc4f99c27a42d
Daumantas Stravinskas <daumantas.stravinskas@gmail.com>
2019-08-12 16:30:37 +0300

    deleted PSAuthorizationUser constructor

--- Sources/Entities/Authorizations/PSAuthorizationUser.swift
+++ Sources/Entities/Authorizations/PSAuthorizationUser.swift
@@ -5,11 +5,6 @@ public class PSAuthorizationUser: Mappable {
     public var userId: Int!
     public var displayName: String!
     
-    public init(userId: Int, displayName: String) {
-        self.userId = userId
-        self.displayName = displayName
-    }
-    
     required public init?(map: Map) { }
     
     public func mapping(map: Map) {
