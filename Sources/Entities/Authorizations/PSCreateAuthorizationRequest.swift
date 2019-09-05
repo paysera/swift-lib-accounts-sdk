1d5a636f420486cde10a8da93b1866f01eb7de2f
Daumantas Stravinskas <daumantas.stravinskas@gmail.com>
2019-09-05 19:25:49 +0300

    entities fixes

--- Sources/Entities/Authorizations/PSCreateAuthorizationRequest.swift
+++ Sources/Entities/Authorizations/PSCreateAuthorizationRequest.swift
@@ -4,6 +4,7 @@ import ObjectMapper
 
 public class PSCreateAuthorizationRequest: Mappable {
     
+    public var id: String?
     public var accountNumber: String!
     public var userIds: [String]!
     public var readPermission = false
@@ -33,7 +34,7 @@ public class PSCreateAuthorizationRequest: Mappable {
     }
 }
 
-public class PSCreateAuthorizationRequestSignPermission {
+public class PSCreateAuthorizationRequestSignPermission: Mappable {
     
     public var level: String!
     public var dayLimit: PSMoney?
