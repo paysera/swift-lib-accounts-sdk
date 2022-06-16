6f37d3c5d932044bb911bc19196aafef54c44b54
Aram Semerjyan <aramsemerjyan@Arams-MacBook-Pro.local>
2022-06-16 08:11:05 +0400

    Change response result model

--- Sources/PayseraAccountsSDK/Entities/PSClientPartnerResponse.swift
+++ Sources/PayseraAccountsSDK/Entities/PSClientPartnerResponse.swift
@@ -1,7 +1,19 @@
 import ObjectMapper
 
+public class PSClientPartnerResponseItem: Mappable {
+    public var code: String!
+    public var covenanteeId: String!
+    
+    required public init?(map: Map) { }
+    
+    public func mapping(map: Map) {
+        code   <- map["code"]
+        covenanteeId <- map["covenantee_id"]
+    }
+}
+
 public class PSClientPartnerResponse: Mappable {
-    public var items: [String]!
+    public var items: [PSClientPartnerResponseItem]!
     
     required public init?(map: Map) { }
     
