452a010c56c9b5963f59420f51061e3b8f4531f3
Aram Semerjyan <aramsemerjyan@Arams-MacBook-Pro.local>
2022-07-07 11:30:54 +0400

    Remove unnecessary comments and mapping

--- Sources/PayseraAccountsSDK/Entities/PSQuestionnaireConfiguration.swift
+++ Sources/PayseraAccountsSDK/Entities/PSQuestionnaireConfiguration.swift
@@ -1,53 +1,11 @@
 import ObjectMapper
 
-//"configuration": {
-//    "is_initial": false,
-//    "feature_flags": [
-//        "natural_questionnaire_activities_information",
-//        "questionnaire_fund_change",
-//        "natural_questionnaire_services",
-//        "questionnaire_multiple_citizenship"
-//    ],
-//    "areas_of_activity": [
-//        {
-//            "id": "1",
-//            "name": "questionnaire.activities.agriculture"
-//        },
-//        {
-//            "id": "2",
-//            "name": "questionnaire.activities.mining"
-//        }
-//        ...
-//    ]
-//}
-
-public final class PSQuestionnaireAreasOfActivity: Mappable {
-    public var id: String
-    public var name: String
-    
-    required public init?(map: Map) {
-        do {
-            id = try map.value("id")
-            name  = try map.value("name")
-        } catch {
-            print(error)
-            return nil
-        }
-    }
-    
-    public func mapping(map: Map) {}
-}
-
 public final class PSQuestionnaireConfigurationItem: Mappable {
     public var isInitial: Bool
-    public var featureFlags: [String]
-    public var areasOfActivity: [PSQuestionnaireAreasOfActivity]
     
     required public init?(map: Map) {
         do {
             isInitial = try map.value("is_initial")
-            featureFlags  = try map.value("feature_flags")
-            areasOfActivity = try map.value("areas_of_activity")
         } catch {
             print(error)
             return nil
